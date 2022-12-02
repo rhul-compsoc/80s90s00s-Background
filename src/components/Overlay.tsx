de5ecf2d2dd56eb09cd46ac9e312b8278b7d7c9b
James <jamesarnott2@gmail.com>
2022-12-02 20:34:41 +0000

    fix: sorted out the logo

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 const Overlay = ({}) => {
   return (
     <div className="fixed w-full h-full flex align-middle items-center">
-      <div className="text-2xl  m-auto">
-        <img src={Logo} alt="Logo" className="compsocLogo p-5" />
+      <div className="text-2xl bottom-0 right-0 fixed">
+        <img src={Logo} alt="Logo" className="p-5 bg-white bg-opacity-75 rounded-tl-2xl h-40" />
       </div>
     </div>
   );
